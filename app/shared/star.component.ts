ddb708263d583b2611bb7a2c1769a922271f6d8d
Machaaouri, Houssame <Houssame.Machaaouri@misys.com>
2017-06-01 19:19:46 +0200

    Using nested component

---
+++ app/shared/star.component.ts
@@ -0,0 +1,21 @@
+import {Component, OnChanges} from '@angular/core';
+
+@Component({
+    selector : 'ai-star',
+    templateUrl: 'app/shared/star.component.html',
+    styleUrls :['app/shared/star.component.css']
+})
+
+
+export class StarComponent implements OnChanges {
+
+    rating : number = 4;
+    startWidth : number;
+
+
+    ngOnChanges(): void {
+
+        this.startWidth = this.rating * 86/5;
+    }
+
+}
