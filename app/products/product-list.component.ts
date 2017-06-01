840b9da290589ee6331bdf8025efdb4d7a340574
Machaaouri, Houssame <Houssame.Machaaouri@misys.com>
2017-06-01 17:33:01 +0200

    handling input with two-way Binding

--- app/products/product-list.component.ts
+++ app/products/product-list.component.ts
@@ -10,6 +10,10 @@ export class ProductListComponent {
     pageTitle: string = 'Product List';
     imageWidth : number = 50;
     imageMargin: number = 2;
+
+    showImage: boolean = false;
+    listFilter: string = 'cart';
+
     products: any[] = [
     
         {
@@ -33,4 +37,9 @@ export class ProductListComponent {
             "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
         }
     ];
+
+
+    toggleImage():void{
+        this.showImage = !this.showImage;
+    }
 }
