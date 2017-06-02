d62fe24110324c6adba7c16dcaf611545c0e987e
Machaaouri, Houssame <Houssame.Machaaouri@misys.com>
2017-06-02 18:33:12 +0200

    Navigation and Routing

---
+++ app/products/product-detail.component.ts
@@ -0,0 +1,31 @@
+import {Component, OnInit} from '@angular/core'
+import {ActivatedRoute,Router} from '@angular/router'
+
+import {IProduct} from './product';
+
+@Component({
+    templateUrl : 'app/products/product-detail.component.html'
+})
+export class ProductDetailComponent implements OnInit
+ {
+
+    pageTitle: String = 'Product Detail';
+    product: IProduct;
+
+    constructor(private _route: ActivatedRoute,
+                private _router: Router){
+
+    }
+
+
+    ngOnInit(): void {
+
+        let id = +this._route.snapshot.params['id'];
+        this.pageTitle += ` : ${id}`;
+    }
+
+    onBack(): void{
+
+        this._router.navigate(['/products']);
+    }
+}
