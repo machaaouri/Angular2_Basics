a7ca21471372e58b2aa03752fbaebe8d30f6a708
Machaaouri, Houssame <Houssame.Machaaouri@misys.com>
2017-06-02 18:49:42 +0200

    complete ProductDetailComponent

--- app/products/product-detail.component.ts
+++ app/products/product-detail.component.ts
@@ -1,7 +1,8 @@
 import {Component, OnInit} from '@angular/core'
 import {ActivatedRoute,Router} from '@angular/router'
-
+import { Subscription }       from 'rxjs/Subscription';
 import {IProduct} from './product';
+import { ProductService } from './product.service';
 
 @Component({
     templateUrl : 'app/products/product-detail.component.html'
@@ -11,21 +12,33 @@ export class ProductDetailComponent implements OnInit
 
     pageTitle: String = 'Product Detail';
     product: IProduct;
+    errorMessage: string;
+    private sub: Subscription;
 
     constructor(private _route: ActivatedRoute,
-                private _router: Router){
+                private _router: Router,
+                private _productService: ProductService){
 
     }
 
 
     ngOnInit(): void {
 
-        let id = +this._route.snapshot.params['id'];
-        this.pageTitle += ` : ${id}`;
+        this.sub = this._route.params.subscribe(
+            params => {
+                let id = +params['id'];
+                this.getProduct(id);
+        });
     }
 
     onBack(): void{
 
         this._router.navigate(['/products']);
+    }
+
+    getProduct(id: number) {
+        this._productService.getProduct(id).subscribe(
+            product => this.product = product,
+            error => this.errorMessage = <any>error);
     }
 }
